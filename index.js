b2319398ed66d62b13b6d47dfc5b6859106c08b6
Joshua <30722988+Mystic-Manatee@users.noreply.github.com>
2018-03-08 13:58:21 +1000

    Added home and about pages, started routing and added ejs for templating

--- index.js
+++ index.js
@@ -11,9 +11,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-//app.use(express.static(path.join(__dirname, 'public/')));
+app.use(express.static(path.join(__dirname, 'public/')));
 
 //routes
 app.get('/', function(req, res){
+	res.render('home');
+});
 
-})
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+
+//app listens on localhost port 3000
+app.listen(3000);
