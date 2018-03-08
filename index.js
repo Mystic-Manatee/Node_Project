a520f03abebe7ac7888e5d3e0bc786d6e8423a89
Joshua <30722988+Mystic-Manatee@users.noreply.github.com>
2018-03-08 12:34:15 +1000

    added modules

---
+++ index.js
@@ -0,0 +1,19 @@
+//setting up required packages
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+
+//setting up middleware for each package
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+//app.use(express.static(path.join(__dirname, 'public/')));
+
+//routes
+app.get('/', function(req, res){
+
+})
